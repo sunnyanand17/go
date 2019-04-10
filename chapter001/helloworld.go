169a09ef184e82f7ff0e1bb4dec088509078584d
sunnyanand17 <Sunny.Anand79@ibm.com>
2019-04-10 12:03:27 -0700

    control flow

--- chapter001/helloworld.go
+++ chapter001/helloworld.go
@@ -4,6 +4,24 @@ import (
 	"fmt"
 )
 
+// entry point to a go program
 func main() {
 	fmt.Println("Hello World")
+
+	// Control flow sequence
+	foo()
+	// Control flow loop;iterattive
+
+	for i := 0; i < 10; i++ {
+		//Control flow conditional
+
+		if i%2 == 0 {
+			fmt.Printf("This is an even number %v \n", i)
+		}
+	}
+
+}
+
+func foo() {
+	fmt.Println("This is the call to the foo function")
 }
