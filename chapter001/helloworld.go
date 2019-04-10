4ae69418506214331af7b4107dae94303219de11
sunnyanand17 <Sunny.Anand79@ibm.com>
2019-04-10 15:12:47 -0700

    Chapter 2

--- chapter001/helloworld.go
+++ chapter001/helloworld.go
@@ -20,76 +20,77 @@ func main() {
 		}
 	}
 	// Print and Scan functions of the fmt package. Lets review them
-	/*
-		//Sample examples below
-		//Genreal Verbs
-		%v	the value in a default format ,when printing structs, the plus flag (%+v) adds field names
-		%#v	a Go-syntax representation of the value
-		%T	a Go-syntax representation of the type of the value
-		%%	a literal percent sign; consumes no value
-		Default format for %v translates to below verbs for the different types
-		bool:                    %t
-		int, int8 etc.:          %d
-		uint, uint8 etc.:        %d, %#x if printed with %#v
-		float32, complex64, etc: %g
-		string:                  %s
-		chan:                    %p
-		pointer:                 %p
+	/* Documentation Link:https://golang.org/pkg/fmt/#Errorf
+	//Sample examples below
+	//Genreal Verbs
+	%v	the value in a default format ,when printing structs, the plus flag (%+v) adds field names
+	%#v	a Go-syntax representation of the value
+	%T	a Go-syntax representation of the type of the value
+	%%	a literal percent sign; consumes no value
+	Default format for %v translates to below verbs for the different types
+	bool:                    %t
+	int, int8 etc.:          %d
+	uint, uint8 etc.:        %d, %#x if printed with %#v
+	float32, complex64, etc: %g
+	string:                  %s
+	chan:                    %p
+	pointer:                 %p
 
 
-		// Boolean
-		%t	the word true or false
+	// Boolean
+	%t	the word true or false
 
-		//integers
-		%b	base 2
-		%c	the character represented by the corresponding Unicode code point
-		%d	base 10
-		%o	base 8
-		%q	a single-quoted character literal safely escaped with Go syntax.
-		%x	base 16, with lower-case letters for a-f
-		%X	base 16, with upper-case letters for A-F
-		%U	Unicode format: U+1234; same as "U+%04X"
+	//integers
+	%b	base 2
+	%c	the character represented by the corresponding Unicode code point
+	%d	base 10
+	%o	base 8
+	%q	a single-quoted character literal safely escaped with Go syntax.
+	%x	base 16, with lower-case letters for a-f
+	%X	base 16, with upper-case letters for A-F
+	%U	Unicode format: U+1234; same as "U+%04X"
 
-		//Floating point and complex constituents
-		%b	decimalless scientific notation with exponent a power of two,
-		in the manner of strconv.FormatFloat with the 'b' format,
-		e.g. -123456p-78
-		%e	scientific notation, e.g. -1.234456e+78
-		%E	scientific notation, e.g. -1.234456E+78
-		%f	decimal point but no exponent, e.g. 123.456
-		%F	synonym for %f
-		%g	%e for large exponents, %f otherwise. Precision is discussed below.
-		%G	%E for large exponents, %F otherwise
+	//Floating point and complex constituents
+	%b	decimalless scientific notation with exponent a power of two,
+	in the manner of strconv.FormatFloat with the 'b' format,
+	e.g. -123456p-78
+	%e	scientific notation, e.g. -1.234456e+78
+	%E	scientific notation, e.g. -1.234456E+78
+	%f	decimal point but no exponent, e.g. 123.456
+	%F	synonym for %f
+	%g	%e for large exponents, %f otherwise. Precision is discussed below.
+	%G	%E for large exponents, %F otherwise
 
-		//String and slice of bytes (treated equivalently with these verbs)
-		%s	the uninterpreted bytes of the string or slice
-		%q	a double-quoted string safely escaped with Go syntax
-		%x	base 16, lower-case, two characters per byte
-		%X	base 16, upper-case, two characters per byte
+	//String and slice of bytes (treated equivalently with these verbs)
+	%s	the uninterpreted bytes of the string or slice
+	%q	a double-quoted string safely escaped with Go syntax
+	%x	base 16, lower-case, two characters per byte
+	%X	base 16, upper-case, two characters per byte
 
-		//Slice
-		%p	address of 0th element in base 16 notation, with leading 0x
+	//Slice
+	%p	address of 0th element in base 16 notation, with leading 0x
 
-		//Pointers
-		%p	base 16 notation, with leading 0x
-		The %b, %d, %o, %x and %X verbs also work with pointers,
-		formatting the value exactly as if it were an integer.
+	//Pointers
+	%p	base 16 notation, with leading 0x
+	The %b, %d, %o, %x and %X verbs also work with pointers,
+	formatting the value exactly as if it were an integer.
 
-		//Compound objects - the elements are printed recursivley using these below rules
-		struct:             {field0 field1 ...}
-		array, slice:       [elem0 elem1 ...]
-		maps:               map[key1:value1 key2:value2 ...]
-		pointer to above:   &{}, &[], &map[]
+	//Compound objects - the elements are printed recursivley using these below rules
+	struct:             {field0 field1 ...}
+	array, slice:       [elem0 elem1 ...]
+	maps:               map[key1:value1 key2:value2 ...]
+	pointer to above:   &{}, &[], &map[]
 
-		// Explicit argument indexes:
-		the notation [n] immediately before the verb indicates that the nth one-indexed argument is to be formatted instead
+	// Explicit argument indexes:
+	the notation [n] immediately before the verb indicates that the nth one-indexed argument is to be formatted instead
 
-		//All errors begin with the string "%!" followed sometimes by a single character (the verb) and end with a parenthesized description.
+	//All errors begin with the string "%!" followed sometimes by a single character (the verb) and end with a parenthesized description.
 
-		//blank identifier is represented by the underscore in go. It can be used to assign values for operands which will be discarded by go harmlessly.
+	//blank identifier is represented by the underscore in go. It can be used to assign values for operands which will be discarded by go harmlessly.
 
 	*/
-	fmt.Sprintf("%[2]d %[1]d\n", 11, 22)
+	s := fmt.Sprintf("%[2]d %[1]d\n", 11, 22)
+	fmt.Print(s)
 }
 
 func foo() {
---
+++ chapter002/chapter002.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Print("Beginning of Chapter 2")
+
+	// Topics -- Tokens, Comments, Identifier, Keywords,Operators & Punctuations
+}
