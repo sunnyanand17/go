87158470ce1f1a790255a00bce23928d9ca7f6f2
sunnyanand17 <Sunny.Anand79@ibm.com>
2019-04-13 14:53:48 -0700

    new learning

--- chapter002/chapter002.go
+++ chapter002/chapter002.go
@@ -52,15 +52,50 @@ func main() {
 
 	//1. Identifier
 	/*Identifiers name program entities such as variables and types. An identifier is a sequence of one or more letters and digits.
-	The first character in an identifier must be a letter.
+	The first character in an identifier must be a letter(unicode letter+ underscore)
 
-	Go has 4 types of Identifiers:
+	Go has 3 types of Identifiers:
 	1) Blank Indentifier: is represented by the underscore character _.
+	2) Predeclared Identifiers are implicitly declared in the universe block:
+		Types:	bool byte complex64 complex128 error float32 float64
+				int int8 int16 int32 int64 rune string
+				uint uint8 uint16 uint32 uint64 uintptr
+
+		Constants:
+				true false iota
+
+		Zero value:
+				nil
+
+		Functions:
+				append cap close complex copy delete imag len
+				make new panic print println real recover
+	3)  Exported Identifiers: An identifier is exported to permit access to it from another package. Two rules are followed
+							  while exporting identifiers:
+		1) the first character of the identifier's name is a Unicode upper case letter (Unicode class "Lu"); and
+		2) the identifier is declared in the package block(declared outside of any function) or it is a field name(in struct field name must start with Capital Letter)
+		   or method name(in an interface or package the method name must start with a Capital Letter).
+
 
 	*/
-	//2. Keywords
+	//2. Keywords are reserved identifiers and cannot be used for program purposes other than they are defined for
+	/*	break        default      func         interface    select
+		case         defer        go           map          struct
+		chan         else         goto         package      switch
+		const        fallthrough  if           range        type
+		continue     for          import       return       var
+	*/
 
 	//3. Operators & Punctuations
+	/*
+		Operators combine operands into expressions
+		Unary Operators: "+" | "-" | "!" | "^" | "*" | "&" | "<-"
+		Relational operators: "==" | "!=" | "<" | "<=" | ">" | ">="
+		Addition Operators: "+" | "-" | "|" | "^"
+		Multiplication Operators: "*" | "/" | "%" | "<<" | ">>" | "&" | "&^"
+		Binary Operators: "||" | "&&" | rel_op | add_op | mul_op
+		Comparisons operators: ==    equal, !=    not equal, <     less ,<=    less or equal ,>     greater ,>=    greater or equal
+	*/
 
 	//4. Literals
 
