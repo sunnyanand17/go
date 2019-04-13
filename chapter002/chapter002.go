aa267cd10983b2335aaeca9a9f8f7ee5fd1f73cc
sunnyanand17 <Sunny.Anand79@ibm.com>
2019-04-12 18:15:22 -0700

    Commit Day2

--- chapter002/chapter002.go
+++ chapter002/chapter002.go
@@ -26,6 +26,42 @@ func main() {
 		hex_digit     = "0" … "9" | "A" … "F" | "a" … "f" .
 	*/
 
-	// Topics -- Tokens, Comments, Identifier, Keywords,Operators & Punctuations
+	// Tokens
+
+	//Comments
+	/* Line Comments - starts with // and stops at the end of line
+	// General Comments - start with the character sequence /* and stop with the first subsequent character sequence
+	Comment cannot start inside a commnet, string or rune.
+	*/
+
+	// There are 4 types of tokens in go
+	//White spaces in go are formed by the follwoing Unicode Code Points:
+	// spaces, horizontal tabs, carriage returns and newline characters
+
+	//Semicolon is the terminator for the go grammar. Go uses the below 2 rules for inserting a semi-colon.
+	/*
+		1. When the input is broken into tokens, a semicolon is automatically inserted into the token stream immediately after a
+			line's final token if that token is:
+			an identifier
+			an integer, floating-point, imaginary, rune, or string literal
+			one of the keywords break, continue, fallthrough, or return
+			one of the operators and punctuation ++, --, ), ], or }
+		2. To allow complex statements to occupy a single line, a semicolon may be omitted before a closing ")" or "}".
+
+	*/
+
+	//1. Identifier
+	/*Identifiers name program entities such as variables and types. An identifier is a sequence of one or more letters and digits.
+	The first character in an identifier must be a letter.
+
+	Go has 4 types of Identifiers:
+	1) Blank Indentifier: is represented by the underscore character _.
+
+	*/
+	//2. Keywords
+
+	//3. Operators & Punctuations
+
+	//4. Literals
 
 }
